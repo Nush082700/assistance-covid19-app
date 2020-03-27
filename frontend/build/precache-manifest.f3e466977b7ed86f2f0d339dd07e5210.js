895af307ea1f835b5f1ec20c14ea0eb2c885d9db
Anusha Choudhury <41936257+Nush082700@users.noreply.github.com>
2020-03-27 05:57:05 -0400

    added build file

---
+++ frontend/build/precache-manifest.f3e466977b7ed86f2f0d339dd07e5210.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4892fc364937b58138ed324457f72910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b75b2a0e85b5abd68e0e",
+    "url": "/static/css/main.0aa7d64b.chunk.css"
+  },
+  {
+    "revision": "d3d7528ab7f3236534e0",
+    "url": "/static/js/2.00039ef2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.00039ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75b2a0e85b5abd68e0e",
+    "url": "/static/js/main.f7ded7ad.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
