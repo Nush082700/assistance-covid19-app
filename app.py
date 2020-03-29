a505ca4bb58897df0e23692b617e9ac5356e0f55
Anusha Choudhury <ac978@cornell.edu>
2020-03-29 13:58:31 -0400

    changed datetime

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ class Todo(db.Model):
     address_help = db.Column(db.String(), nullable=False)
     phone_help = db.Column(db.Integer)
     content = db.Column(db.String(), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.Integer)
     name_helper = db.Column(db.String(), nullable=False)
     address_helper = db.Column(db.String(), nullable=False)
     phone_helper = db.Column(db.Integer)
@@ -108,7 +108,7 @@ def help():
         # pincode_helpee = request.form['pincode']
         new_task = Todo(name_help=name_helpee, content=task_content,
                         address_help=address_helpee, phone_help=phone_helpee,
-                        name_helper="", address_helper="", phone_helper="",pincode = "")
+                        name_helper="", address_helper="", phone_helper="",pincode = "", date_created = "")
 
         try:
             print("Adding to the database")
