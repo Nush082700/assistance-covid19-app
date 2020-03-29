3454668e2555b44ac28d13f5e8ae2da926a1edb9
Anusha Choudhury <ac978@cornell.edu>
2020-03-29 03:24:38 -0400

    made some changes to the main homepage

---
+++ frontend/build/precache-manifest.cfe284a1c6eab39114f41622100ff9de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9efc3b8e7f3f0bb1abad09ad601303d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfe8853873af95876a4d",
+    "url": "/static/css/main.ec3243b9.chunk.css"
+  },
+  {
+    "revision": "6f225bbf8b169352b137",
+    "url": "/static/js/2.8d440701.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8d440701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfe8853873af95876a4d",
+    "url": "/static/js/main.5a4b66c2.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
