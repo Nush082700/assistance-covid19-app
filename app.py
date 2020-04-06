fc1fe3b1dce1c8bf632ce22131c8cb7ac8c1bb33
Anusha Choudhury <ac978@cornell.edu>
2020-04-06 08:09:50 -0400

    made chages to manage latitude-longitude stuff as well as user login

--- app.py
+++ app.py
@@ -7,91 +7,76 @@ import os
 from flask_migrate import Migrate
 from math import radians, cos, sin, asin, sqrt, acos
 from operator import itemgetter
-
-class DictSerializable(object):
-    def _asdict(self):
-        result = OrderedDict()
-        for key in self.__mapper__.c.keys():
-            if type(getattr(self,key)) == str:
-                result[key] = lower(getattr(self, key))
-            else:
-                result[key] = getattr(self, key)
-        return result
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin
 
 
 app = Flask(__name__, static_url_path='',
             static_folder='frontend/build')
 CORS(app)
-# DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(user=POSTGRES_USER,pw=POSTGRES_PW,url=POSTGRES_URL,db=POSTGRES_DB)
-# app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://yvsdbbygynanhv:4621ee9546238883d5abdd451d7a7b6f579e4f8242ca2aa57c8bee4110cc95c7@ec2-54-157-78-113.compute-1.amazonaws.com:5432/d20701ijn8dq6g'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://fzmmhnvdwnhyas:78082b19b58ea424aaddcfa7bc2d87b59610bf9826d2aee0779abb8dac22369a@ec2-18-235-97-230.compute-1.amazonaws.com:5432/d7r9sk576t8up'
-# # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/test'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 # migrate = Migrate(app, db)
-# print(db.Model)
+login = LoginManager(app)
+
+
+
+class User(UserMixin,db.Model):
+    __tablename__ = 'User'
+
+    UserId = db.Column(db.INTEGER, primary_key=True,autoincrement=True)
+    UserName = db.Column(db.String(25),nullable=False, unique=True)
+    Email = db.Column(db.String,nullable=False,unique=True)
+    password_hash = db.Column(db.String, nullable=False)
+    latitude = db.Column(db.Float, nullable = False)
+    longitude = db.Column(db.Float, nullable = False)
+    Help_logs = db.relationship("Todo", backref="Helpee", lazy="dynamic", foreign_keys = 'Todo.helpee_id')
+    Helper_logs = db.relationship("Todo", backref="Helper", lazy="dynamic", foreign_keys = 'Todo.helper_id')
+
+    def __init__(self, UserName, Email, latitude, longitude):
+        self.UserName = UserName
+        self.Email = Email
+        self.latitude = latitude
+        self.longitude = longitude
+    
+    def set_password(self,password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+    
+    
+    def _asdict(self):
+        result = OrderedDict()
+        for key in self.__mapper__.c.keys():
+            result[key] = getattr(self, key)
+        return result
 
 
 class Todo(db.Model):
-    # __tablename__ = 'assistance'
+    __tablename__ = 'assistance'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name_help = db.Column(db.String(), default = "t")
-    # address_help_line1 = db.Column(db.String(), default = "t")
-    # address_help_line2 = db.Column(db.String(), default = "t")
-    # address_help_sector = db.Column(db.String(), default = "t")
-    # address_help_city = db.Column(db.String(), default = "t")
-    # address_help_state = db.Column(db.String(), default = "t")
-    # address_help_country = db.Column(db.String(), default = "t")
-    # address_help_pincode = db.Column(db.Integer, default = 0)
-    lat_help = db.Column(db.Float)
-    long_help = db.Column(db.Float)
-    phone_help = db.Column(db.BigInteger, default = 0)
-    content = db.Column(db.String(), default = "t")
-    name_helper = db.Column(db.String(), default = "t")
-    # address_helper_line1 = db.Column(db.String(), default = "t")
-    # address_helper_line2 = db.Column(db.String(), default = "t")
-    # address_help_sector = db.Column(db.String(), default = "t")
-    # address_helper_city = db.Column(db.String(), default = "t")
-    # address_helper_state = db.Column(db.String(), default = "t")
-    # address_helper_country = db.Column(db.String(), default = "t")
-    # address_helper_pincode = db.Column(db.Integer, default = 0)
-    phone_helper = db.Column(db.BigInteger, default = 0)
-    lat_helper = db.Column(db.Float)
-    long_helper = db.Column(db.Float)
-
+    id = db.Column(db.Integer, primary_key=True, autoincrement = True)
+    helpee_id = db.Column(db.Integer,db.ForeignKey('User.UserId'),nullable=False)
+    name_help = db.Column(db.String())
+    phone_help = db.Column(db.BigInteger)
+    content = db.Column(db.String())
+    helper_id = db.Column(db.Integer,db.ForeignKey('User.UserId'),nullable=False)
+    name_helper = db.Column(db.String())
+    phone_helper = db.Column(db.BigInteger)
     
 
-    # def __init__(self, name_help, phone_help, content, date_created, name_helper, phone_helper,
-    # address_help_line1, address_help_line2, address_help_sector, address_help_city, address_help_state, address_help_country, address_help_pincode,
-    # address_helper_line1, address_helper_line2, address_helper_sector, address_helper_city, address_helper_state, address_helper_country, address_helper_pincode,
-    # lat_help, long_help, lat_helper, long_helper):
-    def __init__(self, name_help, phone_help, content, name_helper, phone_helper,lat_help, long_help, lat_helper, long_helper):
+    def __init__(self, name_help, phone_help, content, name_helper, phone_helper, helpee_id, helper_id):
         self.name_help = name_help
         self.phone_help = phone_help
         self.content = content
         self.name_helper = name_helper
         self.phone_helper = phone_helper
-        # self.address_help_line1 = address_help_line1
-        # self.address_help_line2 = address_help_line2
-        # self.address_help_sector = address_help_sector
-        # self.address_help_city = address_help_city
-        # self.address_help_state = address_help_state
-        # self.address_help_country = address_help_country
-        # self.address_help_pincode = address_help_pincode
-        # self.address_helper_line1 = address_helper_line1
-        # self.address_helper_line2 = address_helper_line2
-        # self.address_helper_sector = address_helper_sector
-        # self.address_helper_city = address_helper_city
-        # self.address_helper_state = address_helper_state
-        # self.address_helper_country = address_helper_country
-        # self.address_helper_pincode = address_helper_pincode
-        self.lat_help = lat_help
-        self.long_help = long_help
-        self.lat_helper = lat_helper
-        self.long_helper = long_helper
+        self.helpee_id = helpee_id
+        self.helper_id = helper_id
         
     def _asdict(self):
         result = OrderedDict()
@@ -101,46 +86,111 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
-    
-    def get_lat_help(self):
-        return self.lat_help
-    
-    def get_long_help(self):
-        return self.long_help
-    
+
     def get_id(self):
         return self.id
 
+@login.user_loader
+def load_user(UserId):
+    return User.query.get(int(UserId))
+
+"""
+Filters all the None objects from a list
+"""
+def filt_None(obj):
+    if obj != None:
+        return True
+    else:
+        return False
+
+"""
+Assuming that the create-react-app/frontend is giving me the username and password.
+If the values match, we return a json with the id of the user and their location(i.e the latitude
+and longitude attached with the user)
+If the values don't match, we return an empty json
+"""
+
+@app.route('/login', methods = ['POST','GET'])
+def login():
+    user_name = request.args.get('UserName',None)
+    pwd = request.form('password', None)
+    user = User.query.filter(UserName=user_name).first()
+    if user is None or not user.check_password(pwd):
+        return jsonify(requests = [])
+    else:
+        login_user(user)
+        return jsnofiy(id = user.UserID, 
+        latitude = user.latitude, longitude = user.longitude)
+
+"""
+SignUp route. It will take the person's details and store it in the database.
+Assumptions made: That latitude and longitude will be form submissions.(Would it be possible
+for you to automatically update two fields on the form called latitude and longitude using
+the Google API).
+It reroutes to login.
+Should it return some json? Should we directly reroute it to home page?
+"""
+@app.route('/signup', methods = ['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        user_name = request.form['UserName']
+        password_= request.form['password']
+        email = request.form['Email']
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        user = User(UserName = user_name, Email = email, latitude = latitude, longitude = longitude)
+        user.set_password(password)
+    try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/login')
+    except Exception as e:
+            return(str(e))
+    
+    else:
+        return app.send_static_file('index.html') #I am not sure what we should have here. Change it accordingly
+
+"""
+Logsout an user from an existing session.
+"""
+@app.route('/logout', methods = ['GET','POST'])
+def logout():
+    logout_user()
+    return redirect('/')
+
+"""
+This was the index file inititally.
+I believe, we need to change the app route as this isn't the base app route(it could be?)
+I am not sure how we'll get data because after login, I am passing the userID, latitude and longitude
+as a json. When the frontend now sends the data, it needs to send the UserID assosciated with that logging session
+for database additions. 
+Everything else is pretty much the same.
+
+"""
 @app.route('/', methods=['POST', 'GET'])
+# @login_required #this decorator prevents us from accessing this app route if we dont have a user logged in.
 def help():
-    # return app.send_static_file('index.html')
     if request.method == 'POST':
         name_helpee = request.form['name_help']
-        # address_helpee_line1 = request.form['address_help_line1']
-        # address_helpee_line2 = request.form['address_help_line2']
-        # address_help_sector = request.form ['address_help_sector']
-        # address_help_city = request.form['address_help_city']
-        # address_help_state = request.form['address_help_state']
-        # address_help_country = request.form['address_help_country']
-        # address_help_pincode = request.form ['address_help_pincode']
         phone_helpee = request.form['phone_help']
         task_content = request.form['content']
-        lat_help = request.form['lat_help']
-        long_help = request.form['long_help']
-        # new_task = Todo(name_help=name_helpee, content=task_content, phone_help=phone_helpee,
-        # address_helpee_line1=address_helpe_line1, address_helpee_line2=address_helpe_line2, 
-        # address_help_sector=address_help_sector,address_help_city=address_help_city,address_help_state=address_help_state,
-        # address_help_country=address_help_country,address_help_pincode=address_help_pincode,
-        # address_helper_line1="", address_helper_line="", address_helper_sector="", address_helper_city="",address_helper_state="",
-        # address_helper_country="",address_helper_pincode="",
-        #                 name_helper="", phone_helper=0,lat_help = lat_help, long_help=long_help, lat_helper = 0.0, long_helper = 0.0)
-        new_task = Todo(name_help = name_helpee, content=task_content, phone_help = phone_helpee, 
-        lat_help = lat_help, long_help = long_help, name_helper="", phone_helper=0, lat_helper=0.0, long_helper=0.0)
+        """if the method is POST for storing the id of the user, then use the line below"""
+        user_id = request.form['UserID']
+        """
+        if the method is GET, remove the above if statement and use the lines below. 
+        Change the values of name_help, phone_help etc depending of what you are returning
+        I didn't make phone_help a compulsory field. Can always change that
+        """
+        name_helpee = request.args.get('name_help', None)
+        phone_helpee = request.args.get('phone_help', 0)
+        task_content = request.args.get('content', None)
+        user_id = request.args.get('id', None)
+
+        new_task = Todo(name_help = name_helpee, phone_help = phone_helpee, content = task_content, helpee_id = user_id,
+        name_helper = "", phone_helper = 0, helper_id = 0)
 
         try:
-            print("Adding to the database")
             db.session.add(new_task)
-            print("committing to the database")
             db.session.commit()
             return redirect('/')
         except Exception as e:
@@ -149,6 +199,68 @@ def help():
     else:
         return app.send_static_file('index.html')
 
+
+"""
+The same as app route ('/'). I don't know how we'll store the userid because it will be required
+here too.
+I am adding something for the time being
+"""
+@app.route('/helper/<int:id>', methods=['GET', 'POST'])
+def helper(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.name_helper = request.form['name_helper']
+        task.phone_helper = request.form['phone_helper']
+        task.lat_helper = request.form['lat_helper']
+        task.long_helper = request.form['long_helper']
+        task.helper_id = request.args.get('id', None)
+        #or
+        task.helper_id = request.form['UserId']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "there was an issue in accepting"
+    else:
+        return render_template('helper.html', task=task)
+
+
+@app.route('/requests/all', methods=['GET', 'POST'])
+def getAllRequests():
+    temp = list(Todo.query.all())
+    reqs = list(map(lambda x: x._asdict(), temp))
+    return jsonify(requests=reqs)
+
+"""
+Returns the distance between two pairs of latitudes and longitudes in KM.
+"""
+def get_dist(obj,slong, slat, elong, elat):
+    dist = 6371.01 * acos(sin(slat)*sin(elat) + cos(slat)*cos(elat)*cos(slong - elong))
+    return (obj,dist)
+    
+"""
+returns the json of the objects sorted by ascending order of closest distance
+Again, I don't know how the userid will be stored and passed along to different webpages.
+As the User class is now storing latitude and longitude, the way of getting it changes
+but everything else should stay the same.
+"""
+@app.route('/requestsLatLong', methods =['GET','POST'])
+def closest_points():
+    user = User.query.get_or_404(UserId = id)
+    lat_helper = user.latitude
+    long_helper = user.longitude
+    lst_objects = list(Todo.query.all())
+    fin_vals = sorted(list(map(lambda x: get_dist(x,x.get_long_help(), x.get_lat_help(), long_helper, lat_helper), lst_objects)), key=itemgetter(1))
+    n_lst = [x[0] for x in fin_vals]
+    reqs = list(map(lambda x:x._asdict(),n_lst))
+    return jsonify(requests=reqs)
+
+if __name__ == "__main__":
+    app.run()
+
+"""
+Commenting delete and update for now as it will change later
 
 @app.route('/delete/<int:id>',  methods=['POST', 'GET'])
 def delete(id):
@@ -178,80 +290,7 @@ def update(id):
     else:
         return render_template('update.html', task=task)
 
-
-@app.route('/helper/<int:id>', methods=['GET', 'POST'])
-def helper(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.name_helper = request.form['name_helper']
-        # task.address_helper_line1 = request.form['address_helper_line1']
-        # task.address_helper_line2 = request.form['address_helper_line2']
-        # task.address_helper_sector = request.form['address_helper_sector']
-        # task.address_helper_city = request.form['address_helper_city']
-        # task.address_helper_state = request.form['address_helper_state']
-        # task.address_helper_country = request.form['address_helper_country']
-        # task.address_helper_pincode = request.form['address_helper_pincode']
-        task.phone_helper = request.form['phone_helper']
-        task.lat_helper = request.form['lat_helper']
-        task.long_helper = request.form['long_helper']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return "there was an issue in accepting"
-    else:
-        return render_template('helper.html', task=task)
-
-
-@app.route('/requests/all', methods=['GET', 'POST'])
-def getAllRequests():
-    temp = list(Todo.query.all())
-    reqs = list(map(lambda x: x._asdict(), temp))
-    return jsonify(requests=reqs)
-
 """
-Returns the distance between two pairs of latitudes and longitudes in KM.
-"""
-def get_dist(obj,slong, slat, elong, elat):
-    dist = 6371.01 * acos(sin(slat)*sin(elat) + cos(slat)*cos(elat)*cos(slong - elong))
-    return (obj,dist)
-    
-"""
-return the json of the objects sorted by ascending order of closest distance
-"""
-@app.route('/requestsLatLong', methods =['GET','POST'])
-def closest_points():
-    lat_helper = request.args.get('lat_helper', None)
-    long_helper = request.args.get('long_helper', None)
-    lst_objects =  list(Todo.query.all())
-    fin_vals = sorted(list(map(lambda x: get_dist(x,x.get_long_help(), x.get_lat_help(), long_helper, lat_helper), lst_objects)), key=itemgetter(1))
-    n_lst = [x[0] for x in fin_vals]
-    reqs = list(map(lambda x:x._asdict(),n_lst))
-    return jsonify(requests=reqs)
 
-if __name__ == "__main__":
-    app.run()
-
-"""
-COMMENTED OUT STUFF
-
-I have created a more detailed address set for which we need to be very specific on what
-should be entered. The prompts should be as such:
-Line 1 -> House Number / Flat + Tower Number
-Line 2 -> Street / Society
-Sector -> Sector / county
-city, state, country -> self explanatory
-
-@app.route('/area/<string:line2>', methods=['GET', 'POST'])
-def getPincode(line2):
-    if Todo.query.filter_by(address_help_line2=line2).count() > 0:
-        logs = list(Todo.query.filter_by(address_help_line2=line2))
-        reqs = list(map(lambda x: x._asdict(), logs))
-        return jsonify(requests=reqs)
-    else:
-        return jsonify(requests=[])
-"""
 
 
